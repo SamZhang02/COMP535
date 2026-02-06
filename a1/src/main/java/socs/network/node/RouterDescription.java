bffa38ab706e8d02d2ebfcf67b952133d2af019d
Sam Zhang <sam.zijing.zhang@gmail.com>
2026-02-05 20:04:34 -0500

    Commit a1

---
+++ a1/src/main/java/socs/network/node/RouterDescription.java
@@ -0,0 +1,21 @@
+package socs.network.node;
+
+public class RouterDescription {
+  //used to socket communication
+  String processIPAddress;
+  short processPortNumber;
+  //used to identify the router in the simulated network space
+  String simulatedIPAddress;
+  //status of the router
+  RouterStatus status;
+
+  public void print(){
+    System.out.println("========================================");
+    System.out.printf("Process IP     : %s%n", processIPAddress);
+    System.out.printf("Process port   : %h%n", processPortNumber);
+    System.out.printf("Simulated IP   : %s%n", simulatedIPAddress);
+    System.out.println("========================================");
+  }
+}
+
+
